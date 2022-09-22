0f967f235ae53f84d076a9bcc90f9210437e69bc
wiweka24 <yogomu24@gmail.com>
2022-09-22 22:17:14 +0700

    create basic crud for user model

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const userRoutes = require('./routes/user')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+app.use(bodyParser.json())
+
+
+//Route
+app.use("/users", userRoutes)
+app.get("/", (req, res) => res.send("Welcome to the Users API!"))
+app.all("*", (req, res) => res.send("You've tried reaching a route that doesn't exist."))
+
+//dB
+mongoose
+  .connect(process.env.dB_connection) 
+  .then(console.log("Connected to database"))
+  .catch((error) => console.error(error))
+
+//Server
+app.listen(process.env.PORT, () => 
+  console.log(`Server running on port: http://localhost:${process.env.PORT}`)
+)
