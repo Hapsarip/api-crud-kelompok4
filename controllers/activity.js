fb80d1efa451faf68355c42af65e244181e64741
viraayuoktvn <viraayu01@mail.ugm.ac.id>
2022-09-25 16:07:23 +0700

    menambahkan controller activity

--- controllers/activity.js
+++ controllers/activity.js
@@ -1,3 +1,28 @@
 const userSchema = require ('../models/user')
 const activitySchema = require ('../models/activity')
 
+/*
+Get All Activities from Database
+Path Name: server/user/{ user id }/activities
+*/
+export const getActivities = async (req, res) => {
+    try {
+      const activities = await activitySchema.find()
+      res.json(activities)
+    } catch(err) {
+      res.json({ message : err.message })
+    }
+}
+
+/*
+Get Specific Activity from Database by their Id
+Path Name: server/users/{ user id }/activity/{ activity id}
+*/
+export const getActivity = async (req, res) => {
+    try {
+      const activityById = await activitySchema.findById( req.params.id )
+      res.json(activityById)
+    } catch(err) {
+      res.json({ message : err.message })
+    }
+}
