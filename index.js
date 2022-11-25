9a6c9a5130bd4a35e36e313ae727be56b7c26a41
wiweka24 <yogomu24@gmail.com>
2022-11-25 10:10:17 +0700

    try vercell

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ require('dotenv').config()
 const app = express()
 const corsConfig = {
   credentials: true,
-  origin: true,
+  origin: "http://localhost:3000",
 };
 app.use(cors(corsConfig));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
